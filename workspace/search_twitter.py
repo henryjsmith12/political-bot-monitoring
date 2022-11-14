87bafe109674eb9c24930cb7962f3393bd6bc276
Henry Smith <henryjsmith12@outlook.com>
2022-11-14 08:42:57 -0600

    Update workflows for tweet searches

--- workspace/search_twitter.py
+++ workspace/search_twitter.py
@@ -15,9 +15,25 @@ def searchTwitter(query, tweet_fields, user_fields, next_token, bearer_token):
 
     if response.status_code == 429:
         print("Request limit reached.")
-        time.sleep(sec=1800)
+        time.sleep(secs=1800)
         response = requests.request("GET", url, headers=headers)
 
+
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+
+    return response.json()
+
+
+def searchTwitterCounts(query, tweet_fields, user_fields, next_token, bearer_token):
+    
+    headers = {"Authorization": f"Bearer {bearer_token}"}
+    if next_token is not None:
+        url = f"https://api.twitter.com/2/tweets/counts/recent?query={query}&{tweet_fields}&{user_fields}&next_token={next_token}"
+    else:
+        url = f"https://api.twitter.com/2/tweets/counts/recent?query={query}&{tweet_fields}&{user_fields}"
+
+    response = requests.request("GET", url, headers=headers)
 
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
